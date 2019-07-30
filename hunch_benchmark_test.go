0e379b91d96372fe613d081431e52afea9c3d8e5
AaronJan <aaronjan@qq.com>
2019-07-30 21:42:46 +0800

    improve test coverage

--- hunch_benchmark_test.go
+++ hunch_benchmark_test.go
@@ -5,6 +5,21 @@ import (
 	"testing"
 )
 
+func BenchmarkTakeWithOnlyOne(b *testing.B) {
+	rootCtx := context.Background()
+
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		Take(
+			rootCtx,
+			1,
+			func(ctx context.Context) (interface{}, error) {
+				return 1, nil
+			},
+		)
+	}
+}
+
 func BenchmarkTakeWithFiveExecsThatNeedsOne(b *testing.B) {
 	rootCtx, cancel := context.WithCancel(context.Background())
 	defer cancel()
